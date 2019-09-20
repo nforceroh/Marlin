aed945d8fbb50bb9ea6778cc9874e57cb008e451
Sylvain Martin <sylvain@nforcer.com>
2019-09-20 09:27:50 -0400

    increased z max feedrate

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -725,7 +725,7 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4[, E5]]]]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 1000, 1000, 5, 25 }
+#define DEFAULT_MAX_FEEDRATE          { 1000, 1000, 25, 25 }
 
 /**
  * Default Max Acceleration (change/s) change = mm/s
