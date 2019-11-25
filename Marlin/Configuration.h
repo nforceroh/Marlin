5a8c277cad6eee6e49972157e258dfc729c6f629
Sylvain Martin <sylvain@nforcer.com>
2019-11-24 21:13:20 -0500

    tweak

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -718,7 +718,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 375.66, 416.66 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 375.66, 476.18  }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -1242,8 +1242,8 @@
 
   //#define MESH_EDIT_GFX_OVERLAY   // Display a graphics overlay while editing the mesh
 
-  #define MESH_INSET 1              // Set Mesh bounds as an inset region of the bed
-  #define GRID_MAX_POINTS_X 10      // Don't use more than 15 points per axis, implementation limited.
+  #define MESH_INSET 30              // Set Mesh bounds as an inset region of the bed
+  #define GRID_MAX_POINTS_X 3      // Don't use more than 15 points per axis, implementation limited.
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   #define UBL_MESH_EDIT_MOVES_Z     // Sophisticated users prefer no movement of nozzle
