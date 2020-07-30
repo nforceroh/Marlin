717e9ba6288e7909a03fb1de46f6b4cb28b30f77
Sylvain Martin <sylvain@nforcer.com>
2020-07-30 10:28:57 -0400

    tweaks

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -747,14 +747,14 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 1502.83, 472 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 375.70, 472 }
 
 /**
  * Default Max Feed Rate (mm/s)
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 300, 300, 12, 120 }
+#define DEFAULT_MAX_FEEDRATE          { 300, 300, 30, 120 }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
